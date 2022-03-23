fee56e582145a09fa7737a03c269d1fe9542a9df
pietrogervasoni <80449911+pietrogervasoni@users.noreply.github.com>
2022-03-23 17:34:24 +0100

    add files

---
+++ Libreria.h
@@ -0,0 +1,245 @@
+#pragma once
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+#include <time.h>
+#include <cstdlib>
+
+
+using namespace std;
+
+
+void leyup(int);
+int max(int[], int);
+int min(int[], int);
+int somma(int[], int);
+float media(int[], float);
+float lunghezza_float (float);
+void array_rand (int[], int, int, int);
+void char_ascii (char);
+void ascii_char (int);
+void ascii_char_array (int [], int);
+bool primo (int);
+void print_array (int[], int);
+bool pari (int);
+void selection_sort(int [], int);
+int locate(int [], int, int);
+
+
+
+
+void leyup(int lung) {
+	
+	//pulisce lo schermo
+	system("cls");
+	
+	//scrive la  prima riga
+	for (int i = 0; i < lung; i++) {
+		cout << "*";
+	}
+
+	cout << endl;
+
+	cout << "*";
+	
+	//inserisce gli spazzi prima della parola
+	for (int s = 0; s < ((lung - 3 - 2) / 2); s++) {
+		cout << " ";
+	}
+	cout << "yes";
+	
+	//inserisce gli spazzi dopo della parola
+	for (int d = 0; d < ((lung - 3 - 2) / 2); d++) {
+		cout << " ";
+	}
+	cout << "*" << endl;
+
+	//fa l'ultima riga
+	for (int i = 0; i < lung; i++) {
+		cout << "*";
+	}
+	cout << endl;
+	cout << endl;
+	cout << endl;
+}
+
+int max(int array [], int n) {
+	
+	//imposta come massimo il primo numero e poi controlla se i successivi sono maggiori
+	//se si li imposta coma massimo
+	int max = array[0];
+	for (int i = 1; i < n; i++) {
+		if (array[i] > max) {
+			max = array[i];
+		}
+	}
+	return max;
+}
+
+int min(int array [], int n) {
+	
+	//imposta come minimo il primo numero e poi controlla se i successivi sono maggiori
+	//se si li imposta coma minimo
+	int min = array[0];
+	for (int i = 1; i < n; i++) {
+		if (array[i] < min) {
+			min = array[i];
+		}
+	}
+	return min;
+}
+
+int somma(int array [], int n) {
+	
+	//imposta la somma a 0 e poi somma volta per volta gli elementi di un arrey
+	int somma = 0;
+	for (int i = 0; i < n; i++) {
+		somma = somma + array[i];
+	}
+	return somma;
+}
+
+float media(int array [], float n) {
+	
+	//stramite la somma calcola la media dividendola per il numero di elementi dell'arrey della somma
+	float media = 0;
+	media = somma(array, n) / n;
+	return media;
+}
+
+float lunghezza_float (float num) {
+	
+	int decim = 10;
+	
+	/*
+	13,46427527527754768675
+				
+	13,4642752752
+	*/
+	
+	//tramite il precision scrive il float di lunghezza decim
+	cout.precision(decim);
+	return num;
+}
+
+void array_rand (int array [], int n, int max, int min) {
+	
+	//tramite la funzione rand dare alle celle di un arrey valore casuale
+	
+	srand (time (0));
+	for (int i = 0; i < n; i++) {
+		
+		array[i] = min + rand() % (max - min);
+	}
+}
+
+void char_ascii (char carattere) {
+	
+	//da un carattere resttiuisce il codice ascii
+	//tramite una conversione
+	cout << (int) carattere;
+}
+
+void ascii_char_array (int ascii) {
+	
+	//restituire dal codisce ascii il carattere
+	//tramite una conversione
+	cout << (char) ascii;
+}
+
+void ascii_char_array (int array [], int n) {
+
+	//restituire dal codisce ascii il carattere dei valori in un arrey
+	//tramite una conversione
+	for (int i = 0; i < n; i++) {
+		cout << (char)array[i];
+	}	
+}
+
+bool primo (int n) {
+	
+	//calcolare se un valore e` primo
+	bool primo;
+	int ndiv = 0;
+	
+	for (int i = 1; i <= n; i++) {
+		
+		if (i % n == 0) {
+			
+			ndiv++;
+		}
+	}
+	if (ndiv == 2) {
+		
+		primo = true;
+	}
+	else {
+		
+		primo = false;
+	}
+	return primo;
+}
+
+void print_array (int array [], int lung) {
+	
+	//stampa un vettore
+	for (int i = 0; i < lung; i++) {
+		cout << array[i] << " ";
+	}
+}
+
+bool pari (int n) {
+	
+	bool pari;
+	if (n % 2 == 0) {
+		pari = true;
+	}
+	else {
+		pari = false;
+	}
+	return pari;
+}
+
+void selection_sort (int array [], int n) {
+
+	int min;
+	for (int i = 0; i < n; i++)	{
+
+		min = i;
+		for (int x = i + 1; x < n; x++) {
+
+			if (array[x] < array[min]) {
+
+				min = x;
+			}
+		}	
+		swap(array[i], array[min]);
+	}
+}
+
+int locate (int array [], int dim, int n) {
+
+	selection_sort(array, dim);
+
+	int start, end, mid;
+	end = dim - 1;
+
+	for (start = 0; start <= end; start++) {
+
+		mid = (start + end) / 2;
+		if (array[mid] == n) {
+
+			return mid;	
+		}
+		if (array[mid] < n) {
+
+			start = mid + 1;
+		}
+		else {
+
+			end = mid - 1;
+		}
+	}
+	return -1; //element not found
+}
+
---
+++ README.md
@@ -0,0 +1,103 @@
+<p align="center">
+ <img width=200px height=200px src="libreria.png" alt="Project logo"></a>
+</p>
+
+<h3 align="center">Personal Library</h3>
+
+<div align="center">
+
+[![Status](https://img.shields.io/badge/status-active-success.svg)]()
+
+
+</div>
+
+---
+
+<p align="center"> 
+    <br> 
+</p>
+
+# 📝 Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Usage](#usage)
+- [Built Using](#built_using)
+- [Contributing](../CONTRIBUTING.md)
+- [Authors](#authors)
+
+# 🧐 About
+
+A personal library that contains things of various kinds, some from a school project and some just because i thought they might be useful.
+
+</br>
+
+## Prerequisites
+
+</br>
+
+To use the project file you need the [MinGw](https://www.mingw-w64.org/) or onother compiler for C++.
+
+</br>
+
+## ⬇️Installing
+
+</br>
+
+Clone the git hub repository
+
+Download the zip of the project
+
+Extract the zip and open the project file
+
+edit or compile and run the file
+
+</br>
+
+
+# 🎈 Usage <a name="usage"></a>
+
+`void layup(int)` -> set a layup from the length of a word
+
+`int max(int[], int)` -> calculate the maximum number in an array
+
+`int min(int[], int)` -> calculate the minimum number in an array
+
+`int somma(int[], int)` -> calculate the sum of the number in an array
+
+`float media(int[], float)` -> calculate the average of the number in an array
+
+`float lunghezza_float (float)` -> limits the length of a float
+
+`void array_rand (int[], int, int, int)` -> generate a random array
+
+`void char_ascii (char)` -> convert a char into the correspondent ASCII code 
+
+`void ascii_char (int)` -> convert an ASCII code into a the correspondent char
+
+`void ascii_char_arrey (int [], int)` -> convert every char into the correspondent ASCII code in an array 
+
+`bool primo (int)` -> calculate if the number is prime or not
+
+`void print_arrey (int[], int)` -> print an array
+
+`bool pari (int)` -> calculate if a number is even or not
+
+`void selection_sort (int [], int)` -> order an array in ascending order
+
+`int locate (int [], int, int)` -> locate an int in an array
+
+
+
+# ⛏️ Built Using <a name = "built_using"></a>
+
+- [C++] - Programming Lenguage
+- [MinGw](https://www.mingw-w64.org/) - Compiler
+
+
+
+# ✍️ Authors <a name = "authors"></a>
+
+- [@pietrogervasoni](https://github.com/kylelobo) - Idea & Initial work
+
+
